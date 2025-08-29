75838b21b8186c47e8089fc99c2a6468a1940f54
Mehmet Alp <mehmetsunguralp54@gmail.com>
2025-08-29 04:02:20 +0300

    Prepare deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path';
 // https://vite.dev/config/
 export default defineConfig({
  plugins: [react()],
+ base: '/weather-app-rau-arbeitsschutz/',
  resolve: {
   alias: {
    '@': path.resolve(__dirname, './src'),
